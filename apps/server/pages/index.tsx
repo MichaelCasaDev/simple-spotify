f5f4d44c346a7f634bb5f2dd2555288da4defc76
Michael Casagrande <michael.casa04@gmail.com>
2023-03-04 16:45:27 +0100

    Added favicon to web

--- apps/server/pages/index.tsx
+++ apps/server/pages/index.tsx
@@ -54,6 +54,7 @@ export default function Home() {
     <>
       <Head>
         <title>Sinple Spotify</title>
+        <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
       </Head>
 
       <body>
