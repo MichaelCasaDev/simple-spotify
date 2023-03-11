c9d1a39708a2c741f372b18fd414454fe444335c
Michael Casagrande <michael.casa04@gmail.com>
2023-03-11 10:00:13 +0100

    Removed web APIs
    Decided to call directly Spotify API from clients

--- apps/server/pages/api/[...404].ts
+++
@@ -1,7 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from "next";
-
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  res
-    .status(200)
-    .send(`404 API endpoint not found\nTry ${process.env.BASE_URI}/api/help`);
-}
